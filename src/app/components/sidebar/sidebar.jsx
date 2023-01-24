aba654d82e10acc6f267942de52baff699b84802
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-01-23 16:12:09 -0800

    changed post footer

--- src/app/components/sidebar/sidebar.jsx
+++ src/app/components/sidebar/sidebar.jsx
@@ -4,9 +4,10 @@ const Sidebar = () => {
     return (
         <aside id='sidebar-container' >
             <ul id='sidebar-list-container'>
-                <li className='sidebar-item'><a>First</a></li>
-                <li className='sidebar-item'><a>Second</a></li> {/* Generate these with reddit API */}
-                <li className='sidebar-item'><a>Third</a></li>
+                <li className='sidebar-item'><a>Best</a></li> {/* Add images next to each link? */}
+                <li className='sidebar-item'><a>Hot</a></li> {/* Generate these with reddit API */}
+                <li className='sidebar-item'><a>New</a></li>
+                <li className='sidebar-item'><a>Top</a></li>
             </ul>
         </aside>
     )
--- src/app/features/post/post.jsx
+++ src/app/features/post/post.jsx
@@ -11,9 +11,26 @@ const Post = () => {
             <div id='post-content'>
 
             </div>
-            <div id='post-footer'>
-
-            </div>
+            <menu id='post-footer'>
+                <li className='footer-link'>
+                    <a>
+                        <img src='./src/assets/chat-icon-64px.png' alt='comment icon' />
+                        <span>{'Num'} Comments</span> {/* Add current number of comments */}
+                    </a>
+                </li>
+                <li className='footer-link'>
+                    <a>
+                        <img />
+                        <span>Share</span>
+                    </a>
+                </li>
+                <li className='footer-link'>
+                    <a>
+                        <img />
+                        <span>Save</span>
+                    </a>
+                </li>
+            </menu>
         </article>
     )
 }
