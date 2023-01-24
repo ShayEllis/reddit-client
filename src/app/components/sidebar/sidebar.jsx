c75e4c12235c6498feb5fcc6633892c87d730eff
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-01-23 21:21:16 -0800

    adjusted layout for narrow window width

--- src/app/components/sidebar/sidebar.jsx
+++ src/app/components/sidebar/sidebar.jsx
@@ -2,7 +2,7 @@ import './sidebar.css'
 
 const Sidebar = () => {
     return (
-        <aside id='sidebar-container' >
+        <aside id='sidebar-container' > {/* Add button to open sidebar when window width is below 575px */}
             <ul id='sidebar-list-container'>
                 <li className='sidebar-item'><a>Best</a></li> {/* Add images next to each link? */}
                 <li className='sidebar-item'><a>Hot</a></li> {/* Generate these with reddit API */}
