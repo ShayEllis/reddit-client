569a4fc45f1f7467283c1884eba760e4292c7768
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-01-24 21:08:47 -0800

    finish side bar open/close button

--- src/app/components/sidebar/sidebar.jsx
+++ src/app/components/sidebar/sidebar.jsx
@@ -1,8 +1,54 @@
+import { useEffect, useState } from 'react'
 import './sidebar.css'
+import sidebarButton from '../../../assets/side-bar-arrow.png'
 
 const Sidebar = () => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() =>{
+        function updateWindowWidth () {
+            setWindowWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', updateWindowWidth)
+
+        return () => {
+            window.removeEventListener('resize', updateWindowWidth)
+        }
+    }, [])
+
+    const handleSidebarButtonClick = (event) => {
+        const sidebarButtonClassList = event.target.parentNode.classList
+        const sidebarConatinerClassList = event.target.parentNode.parentNode.classList
+
+        if (windowWidth > 575) {
+            if (!sidebarButtonClassList.contains('button-is-rotated')) {
+                sidebarButtonClassList.remove('button-not-rotated')
+                sidebarConatinerClassList.remove('sidebar-is-open')
+                sidebarButtonClassList.add('button-is-rotated')
+                sidebarConatinerClassList.add('sidebar-is-closed')
+            } else {
+                sidebarButtonClassList.remove('button-is-rotated')
+                sidebarConatinerClassList.remove('sidebar-is-closed')
+            }  
+        } else if (windowWidth <= 575) {
+            if(!sidebarButtonClassList.contains('button-not-rotated')){
+                sidebarButtonClassList.remove('button-is-rotated')
+                sidebarConatinerClassList.remove('sidebar-is-closed')
+                sidebarButtonClassList.add('button-not-rotated')
+                sidebarConatinerClassList.add('sidebar-is-open')
+            } else {
+                sidebarButtonClassList.remove('button-not-rotated')
+                sidebarConatinerClassList.remove('sidebar-is-open')
+            }
+        }
+    }
+
     return (
-        <aside id='sidebar-container' > {/* Add button to open sidebar when window width is below 575px */}
+        <aside className='sidebar-container' > {/* Add button to open sidebar when window width is below 575px */}
+            <button className='open-close-sidebar-button' onClick={handleSidebarButtonClick}>
+                <img src={sidebarButton} alt='open/close sidebar' />
+            </button>
             <ul id='sidebar-list-container'>
                 <li className='sidebar-item'><a>Best</a></li> {/* Add images next to each link? */}
                 <li className='sidebar-item'><a>Hot</a></li> {/* Generate these with reddit API */}
