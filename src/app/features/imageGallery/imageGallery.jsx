f844953375936a2e8ef61cf1c4628434c4a672ae
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-03-04 21:38:24 -0800

    working on image gallery

--- src/app/features/imageGallery/imageGallery.jsx
+++ src/app/features/imageGallery/imageGallery.jsx
@@ -1,15 +1,62 @@
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './imageGallery.css'
 import helpers from '../../functions/helper-functions'
 
 const ImageGallery = (props) => {
+    const prevButton = useRef(null)
+    const nextButton = useRef(null)
     const imgurLink = props.imgurLink
     const singleImageLink = typeof props.images === 'string' && props.images
     const galleryImages = typeof props.images === 'object' && props.images
 
+    const [imageElementArray, setimageElementArray] = useState([])
+    const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
+    useEffect(() => {
+        if (galleryImages) {
+            if (currentImageIndex === 0) {
+                prevButton.current.style.display = 'none'
+                nextButton.current.style.display = 'block'
+            } else if(currentImageIndex === imageElementArray.length -1) {
+                prevButton.current.style.display = 'block'
+                nextButton.current.style.display = 'none'
+            }
+        }
+    }, [currentImageIndex])
+
+
+    useEffect(() => {
+        if (galleryImages) {
+            setimageElementArray(Object.keys(galleryImages).map((image) => {
+                if (galleryImages[image].e === 'Image') {
+                    const imageId = galleryImages[image].id
+                    const imageSizeArray = galleryImages[image].p
+                    const imageFullSize = galleryImages[image].s
+                    if (imageSizeArray) {
+                        return (
+                            <img key={imageId} src={helpers.convertHTMLCodes(imageSizeArray[imageSizeArray.length - 1].u)} />
+                        )
+                    } else if (imageFullSize) {
+                        return (
+                            <img key={imageId} src={helpers.convertHTMLCodes(imageFullSize.u)} />
+                        )
+                    }
+                }
+            }))
+        }
+    }, [galleryImages])
+
+    const handlePrevButtonClick = (event) => {
+        setCurrentImageIndex((pre) => pre - 1)
+    }
+
+    const handleNextButtonClick = (event) => {
+        setCurrentImageIndex((pre) => pre + 1)
+    }
+
     return (
         <>
-            {singleImageLink || imgurLink 
+            {singleImageLink || imgurLink
                 ?
                     <>
                         {singleImageLink && <a href={singleImageLink} target="_blank"><img src={singleImageLink} /></a>}
@@ -17,30 +64,12 @@ const ImageGallery = (props) => {
                     </>
                 :
                     <div className="container">
-                        {Object.keys(galleryImages).map((image, index) => {
-                            if (galleryImages[image].e === 'Image') {
-                                const imageId = galleryImages[image].id
-                                const imageSizeArray = galleryImages[image].p
-                                const imageFullSize = galleryImages[image].s
-                                if (imageSizeArray) {
-                                    return (
-                                            <div className="mySlides" key={imageId}>
-                                                <div className="numbertext"><span>{index + 1}</span></div>
-                                                <a id='img-link' target='_blank'><img src={helpers.convertHTMLCodes(imageSizeArray[imageSizeArray.length - 1].u)} /></a>
-                                            </div>                      
-                                    )
-                                } else if (imageFullSize) {
-                                    return (
-                                            <div className="mySlides" key={imageId}>
-                                                <div className="numbertext"><span>{index + 1}</span></div>
-                                                <a id='img-link' target='_blank'><img src={helpers.convertHTMLCodes(imageFullSize.u)} /></a>
-                                            </div>
-                                    )
-                                }
-                            }
-                        })}
-                        <a id="prev"><span>❮</span></a>
-                        <a id="next"><span>❯</span></a>
+                        <div className="mySlides">
+                            <div className="numbertext"><span>{`${currentImageIndex + 1} / ${imageElementArray.length}`}</span></div>
+                            <a id='img-link' target='_blank'>{imageElementArray[currentImageIndex]}</a>
+                        </div>
+                        <a id="prev" ref={prevButton} onClick={handlePrevButtonClick}><span>❮</span></a>
+                        <a id="next" ref={nextButton} onClick={handleNextButtonClick}><span>❯</span></a>
                     </div>
             }
         </>
